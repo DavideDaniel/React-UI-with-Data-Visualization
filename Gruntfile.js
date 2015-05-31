fe205797872805cd5f6f66a486115583c3e85e93
DavideDaniel <davidedaniel@gmail.com>
2015-05-31 15:59:07 -0700

    grunt and clear

---
+++ Gruntfile.js
@@ -0,0 +1,22 @@
+module.exports = function (grunt) {
+  grunt.initConfig({
+    watch: {
+      react: {
+        files: 'components/*.jsx',
+        tasks: ['browserify']
+      }
+    },
+    browserify: {
+      options: {
+        transform: [require('grunt-react').browserify]
+      },
+      app: {
+        src: 'components/**/*.jsx',
+        dest: 'build/main.js'
+      }
+    }
+  });
+  grunt.loadNpmTasks('grunt-browserify');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.registerTask('default', ['browserify']);
+};
