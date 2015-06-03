66fd7798ee601d196b8daeb2613afdec58c05e2b
DavideDaniel <davidedaniel@gmail.com>
2015-06-03 13:31:57 -0700

    hosting

--- server.js
+++ server.js
@@ -1,25 +1,19 @@
 var path = require('path');
-// var url = require('url');
 var express = require('express');
-// var App         = require('./main');
+var cors = require('cors');
 var app = express();
 // var development = process.env.NODE_ENV !== 'production';
 
 app
-  .use(express.static(__dirname + '/public'))
-  .listen(2000, function () {
-    console.log('Go to http://localhost:2000 in Safari, Firefox or Chrome');
-  });
+  .set('port', process.env.PORT || 8080)
+  .use(cors())
+  .use(express.static(__dirname + '/public'));
 
 app.get('/', function (req, res) {
-    res.sendfile('./public/index.html');
-  })
-  //
-  // if (development) {
-  //   app.get('/assets/bundle.js',
-  //     browserify('./client', {
-  //       debug: true,
-  //       watch: true
-  //     }));
-  // }
-;
+  res.sendfile('./public/index.html');
+});
+
+app.listen(app.get('port'), function () {
+  console.log('Server listening on port %d', app.get(
+    'port'));
+});
